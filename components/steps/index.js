7af4c20e35d7f833fccabe9c79fe5a60006aaa56
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-05-10 20:58:23 +0630

    refactor steps

--- components/steps/index.js
+++ components/steps/index.js
@@ -1,7 +1,40 @@
-export default function Steps({ steps }) {
+export default function Steps({ steps, activeIndex }) {
   return (
-    <div className="steps">
-      <style jsx>{``}</style>
+    <div className="stepper">
+      {steps.map((item, idx) => (
+        <div
+          className="step"
+          key={idx}
+          data-active={activeIndex === idx}
+          data-done={activeIndex > idx}
+        >
+          {item.label}
+        </div>
+      ))}
+      <style jsx>{`
+        .stepper {
+          display: inline-flex;
+          gap: 16px;
+        }
+
+        .step {
+          border: 1px solid transparent;
+          padding: 8px 16px;
+          background-color: lightgray;
+          color: #fff;
+        }
+
+        .step[data-active="true"] {
+          background-color: #fff;
+          color: green;
+          border-color: green;
+        }
+
+        .step[data-done="true"] {
+          background-color: green;
+          color: #fff;
+        }
+      `}</style>
     </div>
   );
 }
--- pages/steps.js
+++ pages/steps.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Button from "../components/Button";
+import Steps from "../components/steps";
 import useSteps from "../lib/steps";
 
 const steps = [
@@ -17,22 +18,11 @@ const steps = [
   },
 ];
 export default function StepsPage() {
-  const { activeIndex, goBack, goNext } = useSteps(steps);
+  const { activeIndex, goBack, goNext } = useSteps(steps.length);
 
   return (
     <main>
-      <div className="stepper">
-        {steps.map((item, idx) => (
-          <div
-            className="step"
-            key={idx}
-            data-active={activeIndex === idx}
-            data-done={activeIndex > idx}
-          >
-            {item.label}
-          </div>
-        ))}
-      </div>
+      <Steps steps={steps} activeIndex={activeIndex}  />
 
       <div>
         <Button text="Cancel" fill={false} onClick={goBack} />
@@ -40,28 +30,7 @@ export default function StepsPage() {
       </div>
 
       <style jsx>{`
-        .stepper {
-          display: inline-flex;
-          gap: 16px;
-        }
-
-        .step {
-          border: 1px solid transparent;
-          padding: 8px 16px;
-          background-color: lightgray;
-          color: #fff;
-        }
-
-        .step[data-active="true"] {
-          background-color: #fff;
-          color: green;
-          border-color: green;
-        }
-
-        .step[data-done="true"] {
-          background-color: green;
-          color: #fff;
-        }
+        
       `}</style>
     </main>
   );
